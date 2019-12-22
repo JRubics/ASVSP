0ca081db7e26bb34c8ab3be53f7275269c4fe31b
Jelena Dokic <jelena.dpk@gmail.com>
2019-12-22 20:18:40 +0100

    bugfix

--- batch/run.sh
+++ batch/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# inace ostane safe mode i ne moze da obrise
+docker exec -it namenode bash -c "hdfs dfsadmin -safemode leave"
+
 docker exec -it spark-master bash -c "hdfs dfs -rm -r -f /projekat*"
 docker exec -it spark-master bash -c "rm -rf /rdd"
 docker cp . spark-master:/rdd
