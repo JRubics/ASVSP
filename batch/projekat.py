d0de51db4ef8fcd7492b07364eb291484543886e
Jelena Dokic <jelena.dpk@gmail.com>
2020-01-15 01:02:42 +0100

    batch

--- batch/projekat.py
+++ batch/projekat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# 2015-01-01 01:00:00	2015	1	1	1	0	23	0	0	151.8	0	0	7	100.0
 
 import os
 from pyspark import SparkContext, SparkConf
@@ -11,6 +12,20 @@ HDFS_NAMENODE = os.environ["CORE_CONF_fs_defaultFS"]
 conf = SparkConf().setAppName("projekat").setMaster("local")
 sc = SparkContext(conf=conf)
 
-text_file = sc.textFile(HDFS_NAMENODE + "/projekat/mini.csv")
-counts = text_file.flatMap(lambda x: pattern.split(x)).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+# text_file = sc.textFile(HDFS_NAMENODE + "/projekat/mini.csv")
+text_file = sc.textFile(HDFS_NAMENODE + "/projekat/dataset_test.csv")
+counts = text_file \
+    .flatMap(lambda line: line.replace(' ', '\t').split(" ")) \
+    .filter(lambda line: float(line.split()[8]) > 0 and float(line.split()[9]) > 0) \
+    .filter(lambda line: float(line.split()[10]) > 70 and float(line.split()[9]) < 110) \
+    .map(lambda line: (int(line.split()[13]), [float(line.split()[8]) / float(line.split()[9]), 1, float(line.split()[11]) / float(line.split()[12]), 1])) \
+    .reduceByKey(lambda a, b: [a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]]) \
+    .map(lambda v: (v[0], [v[1][0]/v[1][1], v[1][2]/v[1][3]]))
 counts.saveAsTextFile(HDFS_NAMENODE + "/projekat/result")
+# .map(lambda line: (int(line.split()[13]), str(float(line.split()[8]) / float(line.split()[9])) + " " + str(float(line.split()[11]) / float(line.split()[12])))) \
+# podeli u redove
+# samo ako ima radijacije
+# i kad je najjaca?
+# vrsta oblaka, ghi, dhi, clratsky -||-
+# suma, broj
+# prosek
---
+++ batch/readme.md
@@ -0,0 +1,6 @@
+When the  amount  of  diffuse  radiation  reaching  the  earth’s  surface is  less  than  or  equal  to  25%  of global  radiation,  the  sky  is termed as clear sky. - kad nema nikakcih cestica, magle...
+-radijacija koja bi bila da nema oblaka
+
+https://www.youtube.com/watch?v=aK0gQOtYZUg
+
+type,0=clear,1=probably clear,2=fog,3=water,4=supercooled water,5=mixed,6=opaque_ice,7=cirrus,8=overlapping,9=overshooting,10=unknown,11=dust,12=smoke
